71b2ca07cab8b852ed552aa753d48f7bb8849879
Lung Chen <perfecthuang413@gmail.com>
2017-03-04 04:04:32 +0800

    change config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongodb: process.env.MONGODB_URI || 'mongodb://localhost:20001, localhost:20002/hotelmart?replicaSet=hotelmart',
-  origin: 'https://nvrdftd.github.io/react-hotelmart'
+  origin: 'https://whispering-gorge-20840.herokuapp.com'
 }

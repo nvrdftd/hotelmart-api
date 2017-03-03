992381a5cde6f188e010ea193f2e84e8d1c84426
Lung Chen <perfecthuang413@gmail.com>
2017-03-04 02:53:26 +0800

    change config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongodb: process.env.MONGODB_URI || 'mongodb://localhost:20001, localhost:20002/hotelmart?replicaSet=hotelmart',
-  origin: 'http://localhost:3000'
+  origin: 'https://thawing-harbor-54155.herokuapp.com'
 }

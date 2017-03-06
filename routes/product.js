fe03c0786313eb5ff647811d3afc483c288c1f5b
Lung Chen <perfecthuang413@gmail.com>
2017-03-06 18:54:31 +0800

    add search feature

--- routes/product.js
+++ routes/product.js
@@ -2,16 +2,29 @@
 
 var express = require('express'),
     router = express.Router(),
-    assert = require('assert'),
     MongoClient = require('mongodb').MongoClient,
     config = require('../config');
 
 MongoClient.connect(config.mongodb)
   .then(db => {
+    let collection = db.collection('product');
+
     router.get('/recommended', (req, res) => {
       const query = {};
-      db.collection('product')
-        .find(query)
+      collection.find(query)
+        .toArray()
+        .then(arr => res.json({ items: arr }))
+        .catch(err => console.log(err));
+    });
+
+    router.post('/search', (req, res) => {
+      let query = {};
+      if (req.body.text) {
+        query = { $text: { $search: req.body.text }};
+      } else if (req.body.category) {
+        query = { category: req.body.category };
+      }
+      collection.find(query)
         .toArray()
         .then(arr => res.json({ items: arr }))
         .catch(err => console.log(err));
